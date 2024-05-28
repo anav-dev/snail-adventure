69c9c29b8bc8ea2cf56c08043fa3b07b79c6d4db
ana <ana@blueberry.ie>
2024-05-28 11:43:50 +0100

    py: Add display instructions and query functions to story.py

--- story.py
+++ story.py
@@ -1,53 +1,59 @@
 # Modules
 import emoji
 from colorama import Fore, Style
+from justifytext import justify
 
-# Colours for text
+# Colours for text | Source: https://pypi.org/project/colorama/
 GRAY = Fore.LIGHTBLACK_EX
-BROWN = "\033[38;2;139;69;19m" #print(BROWN + "text" + Style.RESET_ALL)
+BROWN = "\033[38;2;139;69;19m" #print(COLOUR + "text" + Style.RESET_ALL)
 GREEN = Fore.GREEN
 RED = Fore.RED
 BLUE = Fore.BLUE
+
+# Global variables declaration
+emoji_snail = emoji.emojize(":snail:")
+emoji_whale = emoji.emojize(":whale:")
+brown_dots = BROWN + "..." + Style.RESET_ALL
+blue_dots = BLUE + "..." + Style.RESET_ALL
+username_validated = ""
 
 
 # Functions
 # 1. Welcome message
 def welcome_msg():
     """
-    Displays welcome message.
-
-    Prints welcome content.
+    Prints welcome message.
     """
-    emoji_snail = emoji.emojize(":snail:")
-    emoji_whale = emoji.emojize(":whale:")
-    brown_dots = BROWN + "..." + Style.RESET_ALL
-    blue_dots = BLUE + "..." + Style.RESET_ALL
-
     print("Welcome to the Snail Adventure\n") 
     print("Are you ready to embark on an adventure?\n") 
-    print(f"{brown_dots}{emoji_snail}" + f" {blue_dots}{emoji_whale}\n") 
+    print(f"{brown_dots}{emoji_snail}\n") 
 
 # 2. Get and validate ser's name
 # get user's name
 def get_username():
     """
     Gets name input from user.
+
+    Returns username.
     """
     # input() function takes all the input as a string only
     username = input("What's your name?\n")
 
     if validate_username(username):
-        username = username.capitalize()
-        print(f"{username} is a valid and capitalized username!\n")
+        global username_validated
+        username_validated = username.capitalize()
+        print(f"{username_validated} is a valid username!\n") 
    
-    return username
+    return username_validated
 
 
 # validate user's name
 def validate_username(username):
     """
-    Check user_name entered is a string and is not empty or just whitespace.
-    Raises ValueError if data entered is not valid.
+    Check username entered is a string and is not empty or just whitespace.
+    Raises ValueError if data entered is not valid, and returns False.
+
+    Returns True if data is valid.
     """
     print(f"Validating username: {username} ...\n")
     special_characters = "!@#$%^&*()-+?_=,<>/"
@@ -55,7 +61,7 @@ def validate_username(username):
     try:
         # check if if all characters in username string are alphabetic 
         if username.isalpha():
-            print(f"Valid name: {username}")
+            print(f"{blue_dots}{emoji_whale}\n")
 
         # check if username contains numbers or is empty
         elif username.isdigit() or username.strip() == "":
@@ -67,7 +73,8 @@ def validate_username(username):
 
     except ValueError as e:
         print(f"Invalid name: {e}\n")
-        return False
+        get_username()
+        return False        
     
     return True
     
@@ -75,25 +82,58 @@ def validate_username(username):
 # 3. Display story instructions
 def display_instructions():
     """
-    Prompts user to see instructions.
-
-    Returns True for yes, otherwise False
+    Prints story instructions.
     """
+    instructions = "Snail Adventure is a text-based game where you will engage in a story-driven experience.\nYou will encounter different scenes, challenges, and offer choices.\nBy selecting a choice, such as “help”, “take action,” or “accept offer”,\nyou will move to the next corresponding chapter, advancing the story based on your decisions.\n"
+    instructions_justified = (justify(instructions, 85))
+    
+    for i in instructions_justified:
+        print("\n" + BROWN + i + Style.RESET_ALL + "\n")
+
+# 4. Yes or No Question 
+def query_instructions(username_validated):
+    """
+    Receives the validated username.
+    Prompts user to see story instructions.
+    """
+    answer_yes = ("yes", "y", "ye", "yep", "ok", "okay")
+    answer_no = ("no", "n", "not", "nop", "nope")
+
+    try:
+        print(f"{username_validated}, would you like to see the instructions? (yes/no)\n")
+        query_instructions_answer = input().strip().lower()
+        
+        # if yes answered
+        if query_instructions_answer in answer_yes:
+            #print("yes answered")
+            display_instructions()
+        # if no answered
+        elif query_instructions_answer in answer_no:
+            print("no answered --> call function to start story")
+        # else
+        else:
+            print("Please enter a right answer (yes/no) \n")
+    except ValueError as e:
+        print("Error")
+  
+
+
+
 
 
 
 # read story (start adventure)
-# show story introduction
 # check user's choice
+# display hint
 # redirect user to choosen story part
 # end game -- show tracker info here
 # read again
-# display hint
 # track user's page
 # main function (call all functions)
 def main():
     welcome_msg()
     get_username()
+    query_instructions(username_validated)
 
 main()
 
